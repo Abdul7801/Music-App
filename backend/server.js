8808d6e6374e054bc09e6b1c766938a400a182f3
Abdul Rahim S Bijapur <asfbijapur@gmail.com>
2025-01-29 20:16:11 +0530

    Updated Build

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ mongoose.connect(process.env.MONGO_URI, {
   .catch(err => console.error('MongoDB connection error:', err));
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(path.resolve(), 'build')));
+app.use(express.static(path.join(path.resolve(), '../frontend/dist')));
 
 // Routes
 app.use("/api/v1/auth", authRoutes);
@@ -40,7 +40,7 @@ app.get('/api/v1/songs', getSongs);
 
 // Fallback to index.html for SPA
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve('build/index.html'));
+  res.sendFile(path.resolve(__dirname, '../frontend/dist' ,'index.html'));
 });
 
 // Start the server
